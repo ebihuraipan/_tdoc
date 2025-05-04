bccb531064323ca46ae19c3932a32d55f74890e0
u20 <u20@u20>
2025-05-04 16:52:24 +0900

    vscodeのカラー設定修正

--- _add.sh
+++ _add.sh
@@ -7,9 +7,11 @@ touch "$1/README.md"
 mkdir "$1/.vscode"
 touch "$1/.vscode/settings.json"
 echo '{
-    "titleBar.activeBackground": "#069194",
-    "titleBar.activeForeground": "#000000",
-    "activityBar.background": "#069194",
-    "activityBar.foreground": "#000000"
+    "workbench.colorCustomizations": {
+        "titleBar.activeBackground": "#1e4e13", // タイトルバーの背景色
+        "titleBar.activeForeground": "#fff", // タイトルバーの文字色
+        "activityBar.background": "#1e4e13", // サイドバーの背景色
+        "activityBar.foreground": "#fff", // サイドバーの文字色
+    }
 }' > "$1/.vscode/settings.json"
 
