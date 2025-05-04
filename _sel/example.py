48f1e545298d464a0eee3f4af6b0b3c30d1df2f7
u20 <u20@u20>
2025-05-04 16:40:31 +0900

    selenium環境構築＆起動まで

---
+++ _sel/example.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+# driver = webdriver.Chrome('C:/200_Python/140_Webブラウザー操作(Selenium)/chrome') 
+driver = webdriver.Chrome()
+driver.get('https://www.yahoo.co.jp/')
+time.sleep(2)
+element = driver.find_element(By.LINK_TEXT, "ファイナンス")
+element.click()
+time.sleep(5)
+driver.quit()
